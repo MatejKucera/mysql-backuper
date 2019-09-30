9db321274b987fb6ac28fc13143d7562401e610f
Matěj Kučera <matej.kucera@vse.cz>
2019-09-30 14:24:06 +0200

    fix schedule quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "${SCHEDULE} sh /backup.sh ${HOSTNAME} ${DATABASE} ${USERNAME} ${PASSWORD} ${PORT} >> /var/log/cron.log" >  /etc/cron.d/scheduler
+SCH=$(echo "${SCHEDULE}" | sed -e 's/^"//' -e 's/"$//')
+
+echo $SCH
+
+echo "${SCH} sh /backup.sh ${HOSTNAME} ${DATABASE} ${USERNAME} ${PASSWORD} ${PORT} >> /var/log/cron.log" >  /etc/cron.d/scheduler
 chmod 0644 /etc/cron.d/scheduler
 crontab /etc/cron.d/scheduler
 touch /var/log/cron.log
